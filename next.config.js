7a98bef366189ab5226815b56ccc49309c4d6438
behroz <behrozsedighisf@gmail.com>
2023-04-05 16:59:56 +0430

    config liara

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  output: 'standalone',
+  experimental: {
+    images: {
+      unoptimized: false,
+    },
+  },
 }
 
 module.exports = nextConfig
+
+
